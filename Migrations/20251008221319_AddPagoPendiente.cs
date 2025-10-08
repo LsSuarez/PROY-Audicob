315759b40608ce0b8f41c08137ad58e8ff4a3352
Bry <bryan2910arnold@gmail.com>
2025-10-08 17:56:14 -0500

    cliente abono

---
+++ Migrations/20251008221319_AddPagoPendiente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Audicob.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPagoPendiente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PagoPendiente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Descripcion = table.Column<string>(type: "text", nullable: false),
+                    Monto = table.Column<decimal>(type: "numeric", nullable: false),
+                    FechaVencimiento = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ClienteId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PagoPendiente", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PagoPendiente_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PagoPendiente_ClienteId",
+                table: "PagoPendiente",
+                column: "ClienteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PagoPendiente");
+        }
+    }
+}
